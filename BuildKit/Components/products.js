7428d0b86f57c802b3670e15b17bd269263e6b7b
Edward Jasper <edwardjasper@Edwards-iMac.local>
2017-11-29 09:10:28 -0500

    new images

--- BuildKit/Components/products.js
+++ BuildKit/Components/products.js
@@ -1,55 +1,55 @@
 export let products = [
   {
     title: 'Brand Nude',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/BrandNude_Michelle_grande.jpg?v=1495690321',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/BrandNude_Michelle_grande_96267bb7-3094-46e8-a160-28da73328a7f.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/Brand_Nude_1024_x_1024_grande.jpg?v=1495690321',
     selected: []
   },
   {
     title: 'Brown Bare',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/BrownBare_Ashanti2_large.jpg?v=1495690592',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/BrownBare_Ashanti2_large_728dd530-394b-4b12-966d-812224bc34ba.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/Brown_Bare_1024_x_1024_large.jpg?v=1495690592',
     selected: []
   },
   {
     title: 'Dark Night',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/DarkNight0724_rt_large.jpg?v=1487711728',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/DarkNight0724_rt_large_a4c4d1dc-6115-478c-aed5-7d29b04603a2.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/dark_night_copia_large.jpg?v=1487711728',
     selected: []
   },
   {
     title: 'Dope Taupe',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/DopeTaupe0485_rt_large.jpg?v=1487711885',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/DopeTaupe0485_rt_large_d057f304-8872-4275-bf82-4eebfd5fc109.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/DOPE_TAUPE_copia_large.jpg?v=1487711885',
     selected: []
   },
   {
     title: 'Foxy Brown',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/FoxyBrown0297_rt_large.jpg?v=1487712160',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/FoxyBrown0297_rt_large_53840ca4-1b88-40e2-93ae-d06ac969a8e0.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/foxy_brown_copia_large.jpg?v=1487712160',
     selected: []
   },
   {
     title: 'Mented #5',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/Mented_50853_rt_large.jpg?v=1487712202',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/Mented_50853_rt_large_0aaeffea-a689-4dd4-be97-098b7be99fa5.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/mented_5_copia_large.jpg?v=1487712202',
     selected: []
   },
   {
     title: 'Nude LaLa',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/NudeLaLa0418_rt_large.jpg?v=1487712885',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/NudeLaLa0418_rt_large_1e95c48f-0d25-4932-80d5-3d4be4a6a38e.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/files/Edited_Nude_La_La.jpg?17179014950116231625',
     selected: []
   },
   {
     title: 'Peach Please',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/PeachPlease_Erica_large.jpg?v=1495690482',
+    image1: '//cdn.shopify.com/s/files/1/1583/0411/files/PeachPlease_Erica_large_d5b35154-89e5-48d7-b9a9-a5c3ed2a5f34.jpg?4551692460899286959',
     image2: '//cdn.shopify.com/s/files/1/1583/0411/products/Peach_Please_1024_x_1024_large.jpg?v=1495690482',
     selected: []
   },
   {
     title: 'Pretty in Pink',
-    image1: '//cdn.shopify.com/s/files/1/1583/0411/products/PiP_Ashanti_large.jpg?v=1487713212',
+    image1: '///cdn.shopify.com/s/files/1/1583/0411/files/PiP_Ashanti_large_904c6df2-c669-4372-ac88-f3473542c489.jpg?4551692460899286959',
     image2: 'https://cdn.shopify.com/s/files/1/1583/0411/files/Edited_Pretty_in_Pink.jpg?17179014950116231625',
     selected: []
   }
